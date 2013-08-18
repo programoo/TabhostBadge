458ecc55913b3a7da6ed9c9275255825d3f9ee4b
programoo <mevtwifu@gmail.com>
2013-08-18 21:04:30 +0700

    tabhost with dynamic badge complete
    tabhost with dynamic badge complete

--- gen/com/example/tabhostbadge/R.java
+++ gen/com/example/tabhostbadge/R.java
@@ -21,18 +21,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int circle=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int fragment_textview1=0x7f080005;
-        public static final int fragment_textview2=0x7f080006;
-        public static final int fragment_textview3=0x7f080007;
-        public static final int imageView1=0x7f080003;
+        public static final int action_settings=0x7f08000a;
+        public static final int badge_bg=0x7f080004;
+        public static final int badge_circle=0x7f080005;
+        public static final int badge_count=0x7f080006;
+        public static final int fragment_textview1=0x7f080007;
+        public static final int fragment_textview2=0x7f080008;
+        public static final int fragment_textview3=0x7f080009;
+        public static final int frame_layout=0x7f080003;
         public static final int tab1=0x7f080000;
         public static final int tab2=0x7f080001;
         public static final int tab3=0x7f080002;
-        public static final int textView1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/tabhostbadge/MainActivity.java
+++ src/com/example/tabhostbadge/MainActivity.java
@@ -10,20 +10,23 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout.LayoutParams;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements
 		OnTabChangeListener {
 	String tag = this.getClass().getSimpleName();
 	private FragmentTabHost mTabHost;
-	Tab2Fragment f2 = new Tab2Fragment();
-	
+	TabSpec tab_news;
+	Paint p = new Paint();
+	int new_count=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,51 +37,69 @@ public class MainActivity extends FragmentActivity implements
 		mTabHost.setOnTabChangedListener(this);
 		
 		mTabHost.setup(this, getSupportFragmentManager(), R.id.tab1);
-		mTabHost.addTab(
-				mTabHost.newTabSpec("tab1").setIndicator(getBadgeView("ข่าว",10)),
-				Tab1Fragment.class, null);
+		tab_news = mTabHost.newTabSpec("tab1");
+		tab_news.setIndicator( getBadgeView("1",10 ) ); 
+		mTabHost.addTab(tab_news,Tab1Fragment.class,null);
 
 		mTabHost.setup(this, getSupportFragmentManager(), R.id.tab2);
 		mTabHost.addTab(
-				mTabHost.newTabSpec("tab2").setIndicator(getBadgeView("กล้อง",20)),
+				mTabHost.newTabSpec("tab2").setIndicator(getBadgeView("20",20)),
 				Tab2Fragment.class, null);
 
 		mTabHost.setup(this, getSupportFragmentManager(), R.id.tab3);
 		mTabHost.addTab(
-				mTabHost.newTabSpec("tab3").setIndicator(getBadgeView("วิทยุ",100)),
+				mTabHost.newTabSpec("tab3").setIndicator(getBadgeView("300",100)),
 				Tab3Fragment.class, null);
+		
+		
 
 	}
 
-	public View getBadgeView(String textName,int notification_count) {
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		Log.d(tag,"key press");
 
-		Paint p = new Paint();
-		LayoutInflater lf = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-
-		View v = lf.inflate(R.layout.badge_view, null);
-
+		TextView v = (TextView) mTabHost.getTabWidget().getChildAt(0).findViewById(R.id.badge_count);
+		v.setText(""+new_count++);
+		return super.onKeyDown(keyCode, event);		
+	}
+	public Bitmap getBadgeBitmap(int notification_count){
 		InputStream is = this.getResources().openRawResource(
 				R.drawable.ic_launcher);
 		Bitmap bitmap1 = BitmapFactory.decodeStream(is);
 		Bitmap bmOverlay = Bitmap.createBitmap(bitmap1.getWidth(),
 				bitmap1.getHeight(), bitmap1.getConfig());
-
+		
 		Canvas canvas = new Canvas(bmOverlay);
-		// canvas.setDensity(160);
+		//canvas.setDensity(100);
 		canvas.drawBitmap(bitmap1, 0, 0, null);
-
+		Paint p = new Paint();
 		p.setColor(Color.RED);
 		canvas.drawCircle(10, 10, 10, p);
-
-		p.setStrokeWidth(2);
+		p.setTextSize(10);
+		p.setStrokeWidth(1);
 		p.setStyle(Paint.Style.STROKE);
 		p.setColor(Color.WHITE);
-		canvas.drawText("" + notification_count, 0, p.getTextSize(), p);
+		canvas.drawText("" + notification_count, 0+2, p.getTextSize()+2, p);
 
-		ImageView iv = (ImageView) v.findViewById(R.id.imageView1);
-		iv.setImageBitmap(bmOverlay);
+		
+		
+		return bmOverlay;
+	}
+	public View getBadgeView(String textName,int notification_count) {
+		
+		LayoutInflater lf = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+		View v = lf.inflate(R.layout.badge_view, null);
 
-		TextView tv = (TextView) v.findViewById(R.id.textView1);
+		ImageView badge_bg_imgView = (ImageView) v.findViewById(R.id.badge_bg);
+		badge_bg_imgView.setImageResource(R.drawable.ic_launcher);
+		
+		ImageView circle_bg_imgView = (ImageView) v.findViewById(R.id.badge_circle);
+		circle_bg_imgView.setImageResource(R.drawable.circle);
+		
+		TextView tv = (TextView) v.findViewById(R.id.badge_count);
+		tv.setTextColor(Color.WHITE);
 		tv.setText( textName );
 
 		return v;
